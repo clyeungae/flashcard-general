2772a62ba6ab17210eee4f73fad14772c5975c48
wilson <clyeung0714@gmail.com>
2022-11-04 22:46:59 +0800

    update addcard page with form

--- www/src/pages/AddCardPage.js
+++ www/src/pages/AddCardPage.js
@@ -1,7 +1,38 @@
+import React from 'react';
+import { useForm } from "react-hook-form";
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import FormInputText from '../component/form/FormTextInput';
+
 export default function AddCardPage () {
+  const {
+    control,
+    handleSubmit,
+  } = useForm();
+
+  async function addRecord (data) {
+    console.log(data);
+  }
   return (
-    <div>
-    <h1>add card here</h1>
-  </div>
+    <Container sx={{padding: "5%"}}>
+      <form
+        onSubmit={handleSubmit(addRecord)}
+        style={{display: 'flex', flexDirection: 'column'}}
+      >
+        <FormInputText
+          control={control}
+          name="hint"
+          label="Hint"
+        />
+        <FormInputText
+          control={control}
+          name="answer"
+          label="Answer"
+        />
+        <div style={{alignSelf: 'center'}}>
+        <Button type="submit" variant="contained">submit</Button>
+        </div>
+      </form>    
+    </Container>
   )
 }
