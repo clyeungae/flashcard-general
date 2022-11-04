a66bd2b8e30c5b1711275e4e7a1f8f569a6f131a
wilson <clyeung0714@gmail.com>
2022-11-04 19:21:37 +0800

    update show card page

--- www/src/pages/CardPage.js
+++ www/src/pages/CardPage.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 import * as API from '../lib/api';
+
+let fetching = false;
 
 export default function CardPage () {
   const [hint, setHint] = useState("");
@@ -11,18 +17,47 @@ export default function CardPage () {
   }, []);
 
   async function updateCard () {
-    const resp = await API.getNewCard();
-    if (resp?.data) {
-      setAnswer(resp.data.answer);
-      setHint(resp.data.hint);
+    if (fetching === true) return;
+    try {
+      fetching = true;
+      const resp = await API.getNewCard();
+      if (resp?.data) {
+        setAnswer(resp.data.answer);
+        setHint(resp.data.hint);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      fetching = false;
     }
   }
 
   return (
-    <div>
-    <h1>show card here</h1>
-    <h1>{hint}</h1>
-    <h1>{answer}</h1>
+    <div style={{padding: "5%"}}>
+    <Card sx={{ minWidth: 275, minHeight: 200, display: 'flex', justifyContent: 'center', alignItems: 'center'}} variant="outlined">
+      <CardContent >
+      <Typography variant={show ? "h5" : "h3"} align="center" paragraph="true">
+        {show ? answer : hint}
+      </Typography>
+      </CardContent>
+    </Card>
+    <div style={{justifyContent: 'center', display: 'flex', marginTop: 10}}>
+      {
+        show ? (
+        <Button
+          variant='contained'
+          onClick={() => { setShow(false);}}
+        >
+          Next
+        </Button>) : (
+        <Button
+          variant='contained'
+          onClick={() => { setShow(true);}}
+        >
+          Reveal
+        </Button>)
+      }
+    </div>
   </div>
   )
 }
