fc70e08aecb3e319e0441b5951f61501fec36ae4
wilson <clyeung0714@gmail.com>
2022-11-05 16:31:27 +0800

    update api structure with express router

---
+++ server/src/route/index.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const model = require('../model');
+
+router.use((req, res, next) => {
+  console.log({req});
+  next();
+})
+
+router.get('/', function (req, res) {
+  res.send('server is working');
+});
+
+router.get('/api/flashcard', async (req, res) => {
+  try {
+    const data = await model.selectRandomFlashCard();
+    res.send({data});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('error');
+  }
+})
+
+router.post('/api/flashcard', async (req, res) => {
+  try {
+    const {hint, answer} = req.body;
+    const result = await model.insertFlashCard(hint, answer);
+    res.send({success: 1});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('error');
+  }
+})
+
+module.exports = router;
--- server/src/web-server.js
+++ server/src/web-server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const express = require('express');
 const http = require('http');
 const model = require('./model');
+const route = require('./route');
 
 const port = 3001;
 
@@ -17,19 +18,7 @@ exports.createWebServer = function () {
     credentials: true,
   }))
 
-  app.get('/', function (req, res) {
-    res.send('server is working');
-  });
-
-  app.post('/api/flashcard', async (req, res) => {
-    res.send('hello world');
-  })
-
-  app.get('/api/flashcard', async (req, res) => {
-    const data = await model.selectRandomFlashCard();
-    res.send({data});
-  })
-
+  app.use(route);
   app.use(function (req, res) {res.sendStatus(404);});
 
   app.use(function (err, req, res, next) {
